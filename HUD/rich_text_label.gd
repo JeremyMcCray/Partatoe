c02ef17defe6daa2a6b32b42737dd17e0dda9299
Sam Clay <claysamuelc@gmail.com>
2025-12-12 23:07:16 -0500

    Add autoload features.

--- HUD/rich_text_label.gd
+++ HUD/rich_text_label.gd
@@ -1,10 +1,7 @@
 extends RichTextLabel
 
-var gold_total = 0
-
 func _ready():
-	PlayerGlobal.pick_up_gold.connect(_on_picked_up_loot)
+	PlayerGlobal.money_updated.connect(_on_picked_up_loot)
 
-func _on_picked_up_loot(amount:int):
-	gold_total += amount
-	text = "[right]"+ str(gold_total) +"[/right]"
+func _on_picked_up_loot(amount: int):
+	text = "[right]"+ str(amount) +"[/right]"
